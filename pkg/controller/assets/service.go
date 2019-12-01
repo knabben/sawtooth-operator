a91240a276f2ba6d81a691488e97748fc772e5d6
Amim Knabben <amim.knabben@gmail.com>
2019-11-30 19:06:28 -0500

    Service spec generator

--- pkg/controller/assets/service.go
+++ pkg/controller/assets/service.go
@@ -6,35 +6,54 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// CreateService returns service spec
-func CreateService(serviceNumber string) *corev1.Service {
-	return &corev1.Service{
-		TypeMeta: metav1.TypeMeta{
-			Kind: "Service",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: fmt.Sprintf("service-%s", serviceNumber),
-			Namespace: "default",
+// NewService returns Sawtooth TCP ports service
+func (s *Sawtooth) NewService(selector map[string]string) *corev1.Service {
+	typeMeta := metav1.TypeMeta{
+		Kind:       "Service",
+		APIVersion: "v1",
+	}
+	objectMeta := metav1.ObjectMeta{
+		Name:      s.GenerateServiceName(),
+		Namespace: "default",
+	}
 
-		},
+	return &corev1.Service{
+		TypeMeta:   typeMeta,
+		ObjectMeta: objectMeta,
 		Spec: corev1.ServiceSpec{
-			Selector: map[string]string{"version": fmt.Sprintf("sawtooth-%s", serviceNumber)},
+			Selector:  s.GenerateSelector(),
 			ClusterIP: "",
-			Ports: []corev1.ServicePort{
-				{
-					Name: "peer",
-					Port: 8800,
-				},
-				{
-					Name: "component",
-					Port: 4004,
-				},
-				{
-					Name: "http",
-					Port: 8008,
-				},
-			},
+			Ports:     s.GenerateTCPPorts(),
 		},
 	}
+}
+
+// GenerateTCPPorts returns the enabled ports for Sawtooth
+func (s *Sawtooth) GenerateTCPPorts() []corev1.ServicePort {
+	return []corev1.ServicePort{
+		{
+			Name: "peer",
+			Port: 8800,
+		},
+		{
+			Name: "component",
+			Port: 4004,
+		},
+		{
+			Name: "http",
+			Port: 8008,
+		},
+	}
+}
+
+// GenerateSelector generates the correct POD selector
+func (s *Sawtooth) GenerateSelector() map[string]string {
+	return map[string]string{
+		"sawtooth-node": fmt.Sprintf("sawtooth-%d", s.NodeNumber),
+	}
+}
+
+// GetServiceName returns the generated name for Sawtooth node
+func (s *Sawtooth) GenerateServiceName() string {
+	return fmt.Sprintf("service-%d", s.NodeNumber)
 }
